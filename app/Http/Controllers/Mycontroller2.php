2bbb8efd64bf1a50e1e2794bc0fd60894ff3bdb6
Admin <phongnv52@wru.vn>
2018-11-21 19:12:09 +0700

    ffdg

---
+++ app/Http/Controllers/Mycontroller2.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 11/18/2018
+ * Time: 3:59 PM
+ */
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Mycontroller2 extends Controller
+{
+    //
+    public function Xinchao(){
+        echo " Hi ae :v cccccccccccccc !";
+    }
+}
